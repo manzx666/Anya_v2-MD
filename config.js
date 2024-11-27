8648b1def246a80360bd8ba640a14994e7872e4b
ManzXD <aimanmk89@gmail.com>
2024-11-27 20:28:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://manz:aiman123@cluster0z.4wolx.mongodb.net/",
     sessionId: process.env.SESSION_ID || "HYJIBD4i0ior88Pm_A_N_Y_A_V_2_EMPZ",
 
     /**
